37d2cafb37308feec080c15cb46cee255261f125
Joshua <hmlu36@gmail.com>
2018-03-13 10:09:45 +0800

    fix bible data encoding error

--- java/ReadTxtFile_Ver2.java
+++ java/ReadTxtFile_Ver2.java
@@ -1,33 +1,40 @@
+package tkmagiclab;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
 public class ReadTxtFile {
 
-	private static final String FILENAME = "D:\\svnbox\\bible\\Bible\\java\\bible.txt";
+	private static final String INPUT_FILE = "D:\\svnbox\\bible\\java\\bible.txt";
+
+	private static final String OUTPUT_FILE = "D:\\svnbox\\bible\\data\\bible.json";
 
 	public static void main(String[] args) {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(FILENAME), "BIG5"))) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(INPUT_FILE), "BIG5"))) {
 
 			String sCurrentLine;
 			String prevChapter = "";
 			String verse = "";
-			System.out.println("[");
+			println("[");
 			boolean lastVerse = false;
-
+			
 			while ((sCurrentLine = br.readLine()) != null) {
 				int semicolon = sCurrentLine.indexOf(":");
 
 				if (!prevChapter.equals(sCurrentLine.substring(0, semicolon))) {
 
 					if (prevChapter.length() > 0) {
-						System.out.println("]},");
+						println("]},");
 					}
-					System.out.println("{\"" + sCurrentLine.substring(0, semicolon) + "\":[");
+					println("{\"" + sCurrentLine.substring(0, semicolon) + "\":[");
 
 					/*
 					 * System.out.println("{\"chapter\":\"" +
@@ -36,14 +43,17 @@ public class ReadTxtFile {
 					 */
 					lastVerse = true;
 				} else {
-					System.out.println(",");
+					println(",");
 				}
 
 				verse = sCurrentLine.substring(semicolon + 1);
 				// System.out.print("{ \"verser\":" + verse.substring(0,
 				// firstStringIdx(verse)));
-				System.out.print("\"" + StringEscapeUtils.unescapeHtml4(verse.substring(firstStringIdx(verse), verse.length()).replace(" ", "")) + "\"");
-				// System.out.println(verse);
+				if (verse.contains("&#")) {
+					print("\"" + StringEscapeUtils.unescapeHtml4(verse.substring(firstStringIdx(verse), verse.length()).replace(" ", "")) + "\"");					
+				} else {
+					print("\"" + verse.substring(firstStringIdx(verse), verse.length()).replace(" ", "") + "\"");
+				}
 
 				/*
 				 * if (!lastVerse) { System.out.println(","); }
@@ -53,7 +63,7 @@ public class ReadTxtFile {
 				prevChapter = sCurrentLine.substring(0, semicolon);
 			}
 
-			System.out.println("]}]");
+			println("]}]");
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -73,4 +83,18 @@ public class ReadTxtFile {
 	public static boolean isNumeric(String str) {
 		return str.matches("-?\\d+(\\.\\d+)?");
 	}
+	
+	public static void print(String string) { 
+		try {
+			Files.write(Paths.get(OUTPUT_FILE), string.getBytes(), StandardOpenOption.APPEND);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void println(String string) {
+		print(string + "\n");
+	}
+	
 }
